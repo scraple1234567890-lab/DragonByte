4b4ea20d1625c0d08dc01520b7f0751e7d1bf502
scraple1234567890-lab <scraple1234567890@gmail.com>
2026-02-28 11:33:04 -0600

    4f24f24f24f24f

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@ export const SUPABASE_ANON_KEY = "sb_publishable_cz_NiGAj5R5u26Ts2QvyDA_Z3H3rMwA
 // Set ONE of these values to restrict article publishing to a single account.
 //
 // Option A (recommended): the email address for the admin author
-export const ARTICLE_ADMIN_EMAIL = "kennykelly@tx.rr.com";
+export const ARTICLE_ADMIN_EMAIL = "scraple1234567890@gmail.com";
 
 // Option B: the Supabase Auth user UUID for the admin author
-export const ARTICLE_ADMIN_USER_ID = "93b7ef1a-b205-47ed-b557-f19797fb724a";
+export const ARTICLE_ADMIN_USER_ID = "eb77fb73-77b8-493a-878e-664357d64302";
